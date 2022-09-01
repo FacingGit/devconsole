27033df36f3530e795641ee0d61c72163370f3ab
FacingGit (FacingNoOneEXE) <72046783+FacingGit@users.noreply.github.com>
2022-09-01 18:18:14 -0400

    Create Loadstring.js

---
+++ Loadstring.js
@@ -0,0 +1,6 @@
+(function() {
+    var x = document.createElement("script");
+    x.src = "https://rawcdn.githack.com/FacingGit/devconsole/fe9f70b6bb9fda6193525b23e99fbd23fe7a5a5f/index.js";
+    x.onload = alert("Product loaded!");
+    document.head.appendChild(x);
+})()
