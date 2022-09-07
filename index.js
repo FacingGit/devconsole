4d8488d8c32c7467b7f0cdf5fa5114ba4f5027f1
FacingGit (FacingNoOneEXE) <72046783+FacingGit@users.noreply.github.com>
2022-09-07 18:09:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function() {
     var x = document.createElement("script");
-    x.src = "https://raw.githack.com/FacingGit/devconsole/main/folder/script/source/code";
+    x.src = "https://raw.githack.com/FacingGit/devconsole/main/source.js";
     document.head.appendChild(x);
 })()
