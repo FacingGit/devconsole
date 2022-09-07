26e5acb7cb0c2003da86a78eb1b6de8df0bdfd67
FacingGit (FacingNoOneEXE) <72046783+FacingGit@users.noreply.github.com>
2022-09-07 17:21:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-alert("Data Lost")
+(function() {
+    var x = document.createElement("script");
+    x.src = "https://raw.githack.com/FacingGit/devconsole/main/folder/script/source/code";
+    document.head.appendChild(x);
+})()
